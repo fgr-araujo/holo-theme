bd39d0af00f3d9332cfbc6a55ee0494f350d998e
iFgR <ifgr.fagner@gmail.com>
2019-12-02 22:28:39 -0300

    Refactoring structure

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,6 @@
 <style src="./style/general.scss" lang="scss"></style>
 <template>
-  <section id="app">
-    <h1>MY NAME</h1>
-
-    <svg
-      version="1.1"
-      viewBox="0 0 106.89 16.933"
-      xmlns="http://www.w3.org/2000/svg"
-      vector-effect="non-scaling-stroke">
-      <g transform="translate(-25.929 51.192)">
-        <path d="m26.458-50.663h104.51l1.3229 1.3229v14.552h-104.51l-1.3229-1.3721z" fill="#0e3b43" fill-opacity=".47059" stroke="#1d7b8b" stroke-linecap="round" stroke-linejoin="round" stroke-width=".26458"/>
-        <g fill="none" stroke="#70d1e1">
-        <g>
-          <path d="m26.458-37.433v1.3229l1.3229 1.3229 2.6458-1e-5" stroke-width="1.0583"/>
-          <path d="m30.427-34.788h9.2604" stroke-width=".52917"/>
-          <path d="m26.458-37.433v-1.3229" stroke-width=".52917"/>
-        </g>
-        <path d="m39.688-34.788h24.648" stroke-width=".26458px"/>
-        <path d="m124.35-34.788h7.9375v-13.229" stroke-width=".26458px"/>
-        </g>
-        <path d="m26.74-51.188 1.041-4e-3v0.89696l-0.94258 0.95511h-0.90951l0.0041-1.0906z" fill="#70d1e1"/>
-        <g fill="none" stroke="#70d1e1">
-        <path d="m26.458-49.34v3.9687" stroke-width=".52917"/>
-        <path d="m27.781-50.663h7.9375" stroke-width=".26458px"/>
-        <path d="m129.65-34.788 2.6458-1e-5v-1.3229" stroke-width=".52917"/>
-        <path d="m88.635-50.663h42.333l1.3229 1.3229v1.3229" stroke-width=".26458px"/>
-        <g>
-          <path d="m132.29-38.756v-7.9375" stroke-width=".79375"/>
-          <path d="m132.29-48.017v-1.3229l-1.3229-1.3229-9.2604 1e-5" stroke-width="1.0583"/>
-          <path d="m27.781-50.663h3.9687" stroke-width=".52917"/>
-        </g>
-        </g>
-      </g>
-      </svg>
-  </section>
+  <main class="sandiwallet">
+    
+  </main>
 </template>
